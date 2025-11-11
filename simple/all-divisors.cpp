6166b7c014dd787184b0f46537648c3d9c8717fc
Nayvedya Shrivastava <joynnayvedya@gmail.com>
2025-11-12 00:10:40 +0530

    Optimize divisor finding and sort output
    Refactored all-divisors.cpp to improve efficiency and output sorting.

---
+++ simple/all-divisors.cpp
@@ -0,0 +1,42 @@
+//standard way
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,i;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        if(n%i==0) cout << i << endl;
+    }
+}
+//better way
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,i;
+    cin>>n;
+    for(i=1;i<=sqrt(n);i++){
+        if(n%i==0) {
+            cout << i << endl;
+            if(n/i != i) cout<<n/i<<endl;
+        }
+    }
+}
+
+//to sort the output use vector then sort or use set
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    set<int> st;
+    int n,i;
+    cin>>n;
+    for(i=1;i<=sqrt(n);i++){
+        if(n%i==0) {
+             st.insert(i);
+            if(n/i != i) st.insert(n/i);
+        }
+    }
+    for(auto it : st){
+        cout << it << " " << endl;
+    }
+
+}
