9e0849bcfc4ca06d287177d9b61e18ef087f74c0
Nayvedya Shrivastava <joynnayvedya@gmail.com>
2025-11-17 23:50:18 +0530

    Update basic-recursion.cpp

--- basics/recursion/basic-recursion.cpp
+++ basics/recursion/basic-recursion.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-void f(){
-    int g=0;
-    cout << "1" << endl;
-    g++;
-    while(g<10){f();}
+void f(int current,int n){
+    if(current>n) return;
+    cout<<"Name"<<endl;
+    f(current+1,n);
 }
 int main(){
-    f();
+    int n;
+    cin>>n;
+    f(1,n);
 }
